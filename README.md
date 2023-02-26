f68bc181fc89f3c274172eda582b690035fba1a2
Jason <namrrac+git@gmail.com>
2023-02-25 20:31:31 -0800

    It works!!!

--- README.md
+++ README.md
@@ -7,17 +7,28 @@ As a new Java developer, you will be required to familiarize yourself with exist
     * Variables
 * Using Java terminology, break down every piece of the method **convertF2K**.
     * What is the access modifier?
+	fahrenheit
     * Is it a class or object method, how do you know?
+	class, because it says it is static`
     * What is its return type?
+	double
     * What parameters does it require, and what are the parameter(s) datatype(s)?
+	It requires fahrenheit, and the conversion of fahrenheitto celsius and the conversion of celsius to kelvin.  They are all double.
     * Describe for me how the body of the method executes (i.e. its flow of execution).
+	fahrenheit gets converted to celsius and the celsius gets converted to kelvin.
 * Using Java terminology, break down method **getUnitChoice**.
     * What is the access modifier?
+	public
     * Is it a class or object method, how do you know?
+	String, 
     * What is its return type?
+	static
     * What parameters does it require, and what are the parameter(s) datatype(s)?
+	C,F,K,Q string
     * Describe for me how the body of the method executes (i.e. its flow of execution).
+		Its going to print the 4 options for the user to input and pass that to Scanner class input.
     * How do the format specifiers differ than what you've seen before and what do they do?
+		%-4s : %40s - means left justfied, the 4 means the space will be 40 characters wide and the %40 means the space will be 40 characters wide. 
 
 ## Part 2: - Implement Missing Method: convertK2F
 This Part is aimed at teaching you to use methods and code you have to add new functionality to existing code. In this case you will add the missing conversion method, but you don't need to know math :-)
--- src/TempConversion.java
+++ src/TempConversion.java
@@ -1,10 +1,15 @@
+/**
+ * Author Jason Carr
+ * Date 2/25/2023
+ */
+
 import java.util.Scanner;
 
 public class TempConversion {
     static final Scanner input = new Scanner(System.in);
 
     public static double convertC2F(double celsius) {
-        return (celsius * (9.0/5)) + 32.0;
+        return (celsius * (9.0 / 5)) + 32.0;
     }
 
     public static double convertC2K(double celsius) {
@@ -23,22 +28,73 @@ public class TempConversion {
         return kelvin - 273.15;
     }
 
+    public static double convertK2F(double kelvin) {
+        return (kelvin - 273.15) * 9 / 5 + 32;
+    }
+
     public static double getTemp(String unit) {
         System.out.printf("%-40s : ", String.format("Please enter °%s temperature to convert", unit));
         return Double.parseDouble(input.nextLine());
     }
 
-    public static String getUnitChoice() {
-        System.out.printf("%-4s : %40s%n", "C).", "To convert a °C temperature");
-        System.out.printf("%-4s : %40s%n", "F).", "To convert a °F temperature");
-        System.out.printf("%-4s : %40s%n", "K).", "To convert a °K temperature");
-        System.out.printf("%-4s : %40s%n", "Q).", "To quit");
-        return input.nextLine();
-    }
-
+    /**
+     * public static String getUnitChoice() {
+     * System.out.printf("%-4s : %40s%n", "C).", "To convert a °C temperature");
+     * System.out.printf("%-4s : %40s%n", "F).", "To convert a °F temperature");
+     * System.out.printf("%-4s : %40s%n", "K).", "To convert a °K temperature");
+     * System.out.printf("%-4s : %40s%n", "Q).", "To quit");
+     * return input.nextLine();
+     * }
+     */
     public static void main(String[] args) {
-        while(true) {
+        String from;
+        String to;
 
+        while (true) {
+            // Requests a temperature Unit to convert from
+            System.out.print("What would you like to convert from (F/C/K) or press Q to quit: ");
+            String choice = input.nextLine();
+            if (choice.equals("q")) {
+                break;
+            } else {
+                from = choice;
+            }
+
+            // Requests a temperature Unit to convert to
+            System.out.print("What would you like to convert to (F/C/K) or press Q to quit: ");
+            choice = input.nextLine();
+            if (choice.equals("q")) {
+                break;
+            } else {
+                to = choice;
+            }
+
+            String caseStr = (from + to);
+            double temp = getTemp(from); //Passes the "from" variable up to the getTemp method and stores the unit as "temp" variable
+
+            switch (caseStr) {
+                case "cf":
+                    System.out.printf("%.2f °C = %.2f °F%n", temp, convertC2F(temp));
+                    break;
+                case "ck":
+                    System.out.printf("%.2f °C = %.2f K%n", temp, convertC2K(temp));
+                    break;
+                case "fc":
+                    System.out.printf("%.2f °F = %.2f °C%n", temp, convertF2C(temp));
+                    break;
+                case "fk":
+                    System.out.printf("%.2f °F = %.2f K%n", temp, convertF2K(temp));
+                    break;
+                case "kc":
+                    System.out.printf("%.2f K = %.2f °C%n", temp, convertK2C(temp));
+                    break;
+                case "kf":
+                    System.out.printf("%.2f K = %.2f °F%n", temp, convertK2F(temp));
+                    break;
+                default:
+                    System.out.println("Invalid!");
+                    break;
+            }
         }
     }
 }
